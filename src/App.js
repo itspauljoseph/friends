7c1be87def51d1035417317c55d80e48653924de
Paul Joseph <paulmjoseph254@gmail.com>
2023-07-18 09:09:52 +0300

    Finished App.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchfield] = useState('')
 
-export default App;
+  useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {setRobots( users )});
+  }, [])
+  
+  const onSearchChange = (event) => {
+    setSearchfield( event.target.value )
+  }
+
+
+    const filterRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    return !robots.length ?
+      <h1>Loading...</h1> :
+      (
+        <div className='tc'>
+          <h1 className="f1">Robot Friends</h1>
+          <SearchBox searchChange={onSearchChange}/>
+          <Scroll>
+          <CardList robots={filterRobots} />
+          </Scroll>
+        </div>
+      );
+  }
+
+  export default App;
